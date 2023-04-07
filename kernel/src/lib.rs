684340f85e8f7d404d7db1b1af124fb19e1c4d46
Michael Zaikin <mz@pklab.io>
2023-04-07 23:39:57 +0300

    Kernel panics

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -7,8 +7,26 @@ pub mod store;
 pub use error::{Error, Result};
 
 #[cfg(target_arch = "wasm32")]
+fn panic_hook(info: &core::panic::PanicInfo) {
+    let message =
+        if let Some(message) = info.payload().downcast_ref::<std::string::String>() {
+            format!("Kernel panic {:?} at {:?}", message, info.location())
+        } else {
+            let message = info.payload().downcast_ref::<&str>();
+            format!("Kernel panic {:?} at {:?}", message, info.location())
+        };
+    
+    unsafe {
+        host::rollup_core::write_debug(message.as_ptr(), message.len());
+    }
+
+    std::process::abort();
+}
+
+#[cfg(target_arch = "wasm32")]
 #[no_mangle]
 pub extern "C" fn kernel_run() {
     let mut context = crate::context::PVMContext::new(unsafe { host::wasm_host::WasmHost::new() });
+    std::panic::set_hook(Box::new(panic_hook));
     crate::kernel::kernel_run(&mut context);
 }
